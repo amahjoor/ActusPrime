1b310132cfd70b15e6da62e003957e4e4349161d
Arman Mahjoor <77rm7n@gmail.com>
2026-02-27 14:46:47 -0800

    add dotenv, fix factory to single-stage generation

--- engine.js
+++ engine.js
@@ -11,6 +11,7 @@ import { readFileSync, writeFileSync, mkdirSync, existsSync, cpSync } from "fs";
 import { execSync, spawn } from "child_process";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+import "dotenv/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
